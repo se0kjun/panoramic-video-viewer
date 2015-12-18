2d6e3e52df7a589cfd0d45d248c30d4b53d84852
SeokJun Hong <hong921122@gmail.com>
2015-12-18 14:39:13 +0900

    Add Util

---
+++ Assets/Script/Util.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Util {
+    public static Vector3 GetVideoPoint(int pos_x, int pos_y)
+    {
+        //video_plane.transform.
+        return new Vector3(pos_x, pos_y, 0f);
+        //return Vector3.zero;
+    }
+}
