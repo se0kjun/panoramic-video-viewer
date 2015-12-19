c218bcc6e48af601bae1b8d2346bd268ed36220a
SeokJun Hong <hong921122@gmail.com>
2015-12-19 20:59:53 +0900

    singleton class

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,17 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 
-
 public class GameManager : MonoBehaviour {
 	public string XMLFileName;
+    public Camera myCam;
 
 	[HideInInspector]
-	public VideoXMLParser parser;
+	public VideoXMLParser parser = VideoXMLParser.Instance;
 
 	public List<GameObject> cube_plane;
-	private List<MovieTexture> cube_movie;
+	public List<MovieTexture> cube_movie;
     private List<DataWrapper> cube_data;
-    private List<VideoXMLWrapper> video_list;
 
 	[HideInInspector]
 	public static MovieTimer GlobalMovieTimer;
@@ -22,14 +21,16 @@ public class GameManager : MonoBehaviour {
         GlobalMovieTimer = new MovieTimer(0.0f);
 
 		if(XMLFileName != null) {
-			parser = new VideoXMLParser(XMLFileName);
+            parser.LoadXML(XMLFileName);
 			cube_data = parser.ParseXML();
-            video_list = parser.ParseVideoXML();
 		}
 
-        //foreach(VideoXMLWrapper video in video_list)
+        //for(int i =0; i<cube_plane.Count && cube_movie.Count > i ; i++)
         //{
-        //    MovieTexture tmp = new MovieTexture();
+        //    cube_plane[i].GetComponent<Renderer>().material.mainTexture = cube_movie[i];
+        //    MovieTexture tmp = cube_plane[i].GetComponent<Renderer>().material.mainTexture as MovieTexture;
+        //    if (tmp != null)
+        //        tmp.Play();
         //}
 
         foreach (GameObject plane in cube_plane)
@@ -41,23 +42,37 @@ public class GameManager : MonoBehaviour {
                 cube_movie.Add(tmp);
             }
         }
+        Debug.Log(myCam.ScreenToWorldPoint(Vector3.zero));
+        Debug.Log(myCam.ScreenToWorldPoint(new Vector3(10.0f, 10.0f, -20.0f)));
     }
 
     void Update () {
+        Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+        Plane xy = new Plane(Vector3.forward, new Vector3(0, 0, myCam.farClipPlane));
+        float distance;
+        xy.Raycast(ray, out distance);
+        ray.GetPoint(distance);
+        Debug.DrawLine(myCam.transform.position, ray.GetPoint(distance));
+        Vector3 m = Input.mousePosition;
+        Debug.Log(myCam.nearClipPlane);
+        m.z = 5.0f;// myCam.nearClipPlane;
+
+        Debug.Log(myCam.ScreenToWorldPoint(m));
+
         GlobalMovieTimer += Time.deltaTime;
 
-        int curr_frame = GlobalMovieTimer.GetFrame(10);
+        int curr_frame = GlobalMovieTimer.GetFrame(Util.MOVIE_FRAME);
         foreach(DataWrapper data in cube_data)
         {
             if(data.MarkerWrapper.StartFrame < curr_frame && data.MarkerWrapper.EndFrame > curr_frame)
             {
-                data.TrackingState = DataWrapper.TrackState.START;
+                //data.TrackingState = DataWrapper.TrackState.START;
                 MovieHelper.tracking_object.Add(data);
             }
         }
 
         MovieHelper.tracking_object.RemoveAll(item => (item.MarkerWrapper.EndFrame < curr_frame));
-        Debug.Log(curr_frame);
+        //Debug.Log(curr_frame);
         //Debug.Log(GlobalMovieTimer.Seconds.ToString() + ":" + GlobalMovieTimer.Fraction.ToString() + "-----" + curr_frame.ToString());
         //Debug.Log(string.Format("{0:00} : {1:00} : {2:} - {3:}", GlobalMovieTimer.Minutes, (int)GlobalMovieTimer.Seconds, (int)GlobalMovieTimer.Fraction, curr_frame));
     }
--- Assets/Script/parser/VideoXMLParser.cs
+++ Assets/Script/parser/VideoXMLParser.cs
@@ -4,58 +4,72 @@ using System.Collections.Generic;
 using System.Xml;
 
 public class VideoXMLParser {
+    private static readonly VideoXMLParser _instance = new VideoXMLParser();
 	private XmlDocument _XMLDocument;
-	private List<VideoXMLWrapper> _VideoWrapper;
-	private List<MarkerXMLWrapper> _MarkerWrapper;
-	private int _count;
+    public Dictionary<string, VideoXMLWrapper> _video_wrapper;
 
-	public List<VideoXMLWrapper> VideoList {
-		get {
-			return _VideoWrapper;
-		}
+    public static VideoXMLParser Instance
+    {
+        get
+        {
+            if (_instance != null)
+                return _instance;
+            else
+                return null;
+        }
+    }
+
+	private VideoXMLParser() {
+        _video_wrapper = new Dictionary<string, VideoXMLWrapper>();
+        _XMLDocument = new XmlDocument();
 	}
 
-	public List<MarkerXMLWrapper> MarkerList {
-		get {
-			return _MarkerWrapper;
-		}
-	}
+    public void LoadXML(string fileName)
+    {
+        _XMLDocument.Load(fileName);
+    }
 
-	public VideoXMLParser(string xml_file) {
-		_VideoWrapper = new List<VideoXMLWrapper>();
-		_MarkerWrapper = new List<MarkerXMLWrapper>();
-		_XMLDocument = new XmlDocument();
-		_XMLDocument.Load(xml_file);
-	}
-
-	public void ParseXML() {
-		XmlNodeList video_node = _XMLDocument.SelectNodes("/data/videos/video");
+    public List<DataWrapper> ParseXML() {
+        ParseVideoXML();
+        List<DataWrapper> result = new List<DataWrapper>();
 		XmlNodeList marker_node = _XMLDocument.SelectNodes("/data/markers/marker");
 
-		foreach(XmlNode video in video_node) {
+		foreach(XmlNode marker in marker_node) {
+			int markerId = int.Parse(marker.Attributes.GetNamedItem("id").Value);
+			XmlNodeList track_list = marker.ChildNodes;
+            DataWrapper tmp = new DataWrapper(markerId);
+            foreach (XmlNode track in track_list) {
+				string video_id = track.Attributes.GetNamedItem("video").Value;
+				int pos_x = (int)float.Parse(track.Attributes.GetNamedItem("position_x").Value.ToString());
+				int pos_y = (int)float.Parse(track.Attributes.GetNamedItem("position_y").Value.ToString());
+				int frame_id = int.Parse(track.Attributes.GetNamedItem("frame").Value.ToString());
+                VideoXMLWrapper vwrap;
+                _video_wrapper.TryGetValue(video_id, out vwrap);
+				tmp.MarkerWrapper.TrackList.Add (new MarkerWrapper(
+					frame_id, pos_x, pos_y, vwrap
+					));
+			}
+            result.Add(tmp);
+        }
+
+        return result;
+	}
+
+    private void ParseVideoXML()
+    {
+        XmlNodeList video_node = _XMLDocument.SelectNodes("/data/videos/video");
+
+        foreach (XmlNode video in video_node)
+        {
             VideoXMLWrapper tmp = new VideoXMLWrapper(
                 video.InnerText,
                 int.Parse(video.Attributes.GetNamedItem("seq").Value),
                 int.Parse(video.Attributes.GetNamedItem("frame").Value),
                 int.Parse(video.Attributes.GetNamedItem("height").Value),
                 int.Parse(video.Attributes.GetNamedItem("width").Value)
-				);
-			_VideoWrapper.Add (tmp);
-		}
-
-		foreach(XmlNode marker in marker_node) {
-			int markerId = int.Parse(marker.Attributes.GetNamedItem("id").Value);
-			XmlNodeList track_list = marker.ChildNodes;
-			foreach(XmlNode track in track_list) {
-				MarkerXMLWrapper tmp = new MarkerXMLWrapper(markerId);
-				string video_id = track.Attributes.GetNamedItem("video").Value;
-				int pos_x = (int)float.Parse(track.Attributes.GetNamedItem("position_x").Value.ToString());
-				int pos_y = (int)float.Parse(track.Attributes.GetNamedItem("position_y").Value.ToString());
-				int frame_id = int.Parse(track.Attributes.GetNamedItem("frame").Value.ToString());
-				tmp.TrackList.Add (new MarkerWrapper(
-					video_id, frame_id, pos_x, pos_y, _VideoWrapper
-					));
-			}
-		}
-	}
+                );
+            tmp.ObjectPlane = GameObject.Find("Plane (" + video.Attributes.GetNamedItem("seq").Value + ")");
+            _video_wrapper.Add(video.Attributes.GetNamedItem("seq").Value, tmp);
+        }
+    }
 }
