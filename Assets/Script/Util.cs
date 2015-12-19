73b15793ee09590a5d590773a2bc275fb72eee0e
SeokJun Hong <hong921122@gmail.com>
2015-12-19 20:56:13 +0900

    Add method to convert a position of video to a localPosition of plane in unity

--- Assets/Script/Util.cs
+++ Assets/Script/Util.cs
@@ -2,10 +2,21 @@
 using System.Collections;
 
 public class Util {
-    public static Vector3 GetVideoPoint(int pos_x, int pos_y)
+    public static readonly int MOVIE_FRAME = 10;
+    public static readonly int PLANE_WIDTH = 10;
+    public static readonly int PLANE_HEIGHT = 10;
+
+    public static Vector3 GetVideoPoint(VideoXMLWrapper video, int pos_x, int pos_y)
     {
-        //video_plane.transform.
-        return new Vector3(pos_x, pos_y, 0f);
-        //return Vector3.zero;
+        int width = video.Width;
+        int height = video.Height;
+
+        float plane_w_point = ((float)pos_x / (float)width) * Util.PLANE_WIDTH;
+        float plane_h_point = ((float)pos_y / (float)height) * Util.PLANE_HEIGHT;
+
+        float plane_x = (PLANE_WIDTH / 2) - plane_w_point;
+        float plane_z = -(PLANE_HEIGHT / 2) + plane_h_point;
+
+        return new Vector3(plane_x, 0f, plane_z);
     }
 }
