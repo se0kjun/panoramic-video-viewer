6a220c4e6a016a6b09a7c1bdd070f42c2f491acf
SeokJun Hong <hong921122@gmail.com>
2015-12-20 16:49:26 +0900

    OnWillRenderObject

---
+++ Assets/Script/CheckRenderObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckRenderObject : MonoBehaviour {
+    void OnBecameVisible()
+    {
+        Debug.Log("testsetsetsetset");
+    }
+
+    //void OnBecameInvisible()
+    //{
+    //    Debug.Log("eeeee" + Camera.current.name);
+    //}
+    void OnWillRenderObject()
+    {
+        GameManager.outside_camera_object.Remove(gameObject.name);
+
+        //if (Camera.current.name == "CenterEyeAnchor")
+        //{
+        //    GameManager.outside_camera_object.Remove(gameObject.name);
+        //}
+        //else
+        //{
+        //    //if(GameManager.outside_camera_object.ContainsKey(gameObject.name))
+        //    GameManager.outside_camera_object.Add(gameObject.name, gameObject);
+        //}
+    }
+}
